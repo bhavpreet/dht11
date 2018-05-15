625ea5481632195dfc5e1f0b27441592bd3f710a
bhavpreet <bhavpreet@gmail.com>
2018-05-15 19:37:19 +0530

    type and float typecase

--- dht11.go
+++ dht11.go
@@ -35,7 +35,7 @@ func main() {
 	laststate := rpio.High
 	for i := 0; i < 85; i++ {
 		counter := 0
-		for pin.Read() == lsatstate {
+		for pin.Read() == laststate {
 			counter++
 			time.Sleep(1)
 			if counter == 255 {
@@ -60,7 +60,7 @@ func main() {
 
 	if (j >= 40) &&
 		(dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)) {
-		f = dht11_dat[2]*9./5. + 32
+		f = float32(dht11_dat[2]*9./5. + 32)
 		fmt.Printf("Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
 			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f)
 	} else {
