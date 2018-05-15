a56f9d82c92276e2d4eb7078276b0bab003a52cc
bhavpreet <bhavpreet@gmail.com>
2018-05-15 19:31:25 +0530

    initial data sheet

--- dht11.go
+++ dht11.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/stianeikeland/go-rpio"
 )
@@ -10,5 +11,59 @@ func main() {
 	err := rpio.Open()
 	if err != nil {
 		fmt.Println("Some error occured", err)
+		exit(0)
 	}
+
+	defer rpio.Close()
+
+	var (
+		j         uint8
+		f         float
+		dht11_dat [5]int
+	)
+
+	pin := rpio.Pin(7)
+	pin.Output()
+	pin.Low()
+	time.Sleep(18)
+	pin.PullUp()
+	time.Sleep(40)
+
+	pin.Input()
+
+	laststate := rpio.High
+	for i := 0; i < 85; i++ {
+		counter := 0
+		for pin.Read() == lsatstate {
+			counter++
+			time.Sleep(1)
+			if counter == 255 {
+				break
+			}
+		}
+
+		laststate = pin.Read()
+
+		if counter == 255 {
+			break
+		}
+
+		if (i >= 4) && (i%2 == 0) {
+			dht11_dat[j/8] <<= 1
+			if counter > 16 {
+				dht11_dat[j/8] |= 1
+			}
+			j++
+		}
+	}
+
+	if (j >= 40) &&
+		(dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)) {
+		f = dht11_dat[2]*9./5. + 32
+		fmt.Printf("Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
+			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f)
+	} else {
+		fmt.Println("Data not good, skip\n")
+	}
+
 }
